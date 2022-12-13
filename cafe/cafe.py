18c84f1deb4ecb0fe432109fcd1915cf37e0a584
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 16:08:10 +0900

    1

--- cafe/cafe.py
+++ cafe/cafe.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('./cafe/data.csv', encoding='cp949')
-st.write(df.iloc[4588:5129])
+#st.write(df.iloc[4588:5129])
 fig = plt.figure(figsize=(8, 4))
 sns.histplot(data=data, x='상품명')
 st.pyplot(fig)
