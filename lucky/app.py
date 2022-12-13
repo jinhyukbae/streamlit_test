e430169c3729367c9d507e6978becfdeed9bce38
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 12:41:54 +0900

    1

--- lucky/app.py
+++ lucky/app.py
@@ -8,10 +8,11 @@ st.header("Welcome!!") #제목 다음으로 강조할 거
 
 # 추첨 대상 13명의 이름을 넣을 수 있는 text_input
 # 3 x 4 3행 4열 배치
-columns = st.columns(4)
+columns = st.columns(4) #화면을 열로 나누어서 배치
+# 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # co1 co2 col3 col4
-for idx,c in enumerate(columns): 
-    st.text_input("조 추첨 대상", key=idx)
+for idx,col in enumerate(columns): 
+    col.text_input("조 추첨 대상", key=idx)
 # 13명이 소속될 조 이름을 넣을 위치
 
 # <추첨 버튼>
