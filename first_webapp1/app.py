3f06c57f2c81e1a3f59fdaedbe46275a3bceb5de
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 11:45:13 +0900

    streamlit

---
+++ first_webapp1/app.py
@@ -0,0 +1,16 @@
+#streamlit 라이브러리 호출
+import streamlit as st 
+
+#마크다운을 기반으로한 꾸미기 기능 작동 write
+st.write(
+    """
+    # 제 첫 웹페이지 입니다
+    ## 부족하지만 많이 사랑해주세요!
+    * 1$ = 1,300원
+    """
+)
+
+#이미지를 넣고 싶으면 image
+st.image(
+    https://www.google.com/url?sa=i&url=https%3A%2F%2Fkr.freepik.com%2Fphotos%2Fcat&psig=AOvVaw1saewO1kDLorOtD-03mf3R&ust=1670985778088000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPjbj5_J9fsCFQAAAAAdAAAAABAE
+)
