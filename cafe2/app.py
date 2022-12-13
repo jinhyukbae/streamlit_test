8ab329021e960ab063cb805c8f64ff3f07a022ea
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 17:17:48 +0900

    1

--- cafe2/app.py
+++ cafe2/app.py
@@ -10,6 +10,8 @@ st.write(df.iloc[5000:5020])
 #st.write(df.iloc[4588:5129])
 two = df.iloc[4588:5129]
 
+all = two.groupby('판매수량').sum()
+st.write(all)
 # fig = plt.figure(figsize=(10, 10))
 # # #sns.histplot(data=three, x='상품명')
 # # #sns.histplot(x="상품명", y='판매수량', data=three)
