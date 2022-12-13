1b52f27172e7a88059184a2876300a827b351da9
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 16:32:38 +0900

    1

--- cafe2/app.py
+++ cafe2/app.py
@@ -8,7 +8,8 @@ df = pd.read_csv('./cafe2/data.csv', encoding='cp949')
 # st.write(df.iloc[4588:5129])
 two = df.iloc[4588:5129]
 fig = plt.figure(figsize=(8, 4))
-sns.histplot(data=two, x='상품명')
+#sns.histplot(data=two, x='상품명')
+sns.countplot(x="상품명", data=two)
 st.pyplot(fig)
 
 #5000~5019 12.25 크리스마스 음료 판매
