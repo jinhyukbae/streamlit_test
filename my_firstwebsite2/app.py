e228ffac713a6c0390295a83de6cde9f82e1312f
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 12:08:44 +0900

    potato

---
+++ my_firstwebsite2/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write(
+    """
+    # 맛있는 감자탕
+    
+    """
+)
+
+st.image(https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Korean.food-Gamjatang-01.jpg/330px-Korean.food-Gamjatang-01.jpg)
