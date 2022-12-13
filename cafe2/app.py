1e5bcfed9ab72125c845a7cff05d877c839dbf2c
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 17:39:41 +0900

    1

--- cafe2/app.py
+++ cafe2/app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px 
 
 df = pd.read_csv('./cafe2/data.csv', encoding='cp949')
 # three = df.iloc[5000:5020]
@@ -11,3 +12,6 @@ df = pd.read_csv('./cafe2/data.csv', encoding='cp949')
 two = df.iloc[4588:5129]
 st.write(two)
 st.bar_chart(three)
+
+fig = px.histogram(two, x="영업일자")
+st.plotly_chart(fig)
