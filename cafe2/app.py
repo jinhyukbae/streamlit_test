877a5debacaf6758129eaca44204f81983c7c6fb
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 16:20:12 +0900

    help

---
+++ cafe2/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('./cafe2/data.csv', encoding='cp949')
+# st.write(df.iloc[4588:5129])
+two = df.iloc[4588:5129]
+fig = plt.figure(figsize=(8, 4))
+sns.histplot(data=two, x='상품명')
+st.pyplot(fig)
+
+#5000~5019 12.25 크리스마스 음료 판매
+
+#4588 5129
