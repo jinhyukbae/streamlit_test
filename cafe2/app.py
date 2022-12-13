baa315b7e644fb32fa32b713cc0bbc400e566943
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 16:56:10 +0900

    1

--- cafe2/app.py
+++ cafe2/app.py
@@ -6,13 +6,14 @@ import seaborn as sns
 
 df = pd.read_csv('./cafe2/data.csv', encoding='cp949')
 three = df.iloc[5000:5020]
+st.write(df.iloc[5000:5020])
 #st.write(df.iloc[4588:5129])
 # two = df.iloc[4588:5129]
 
-fig = plt.figure(figsize=(10, 10))
-sns.histplot(data=three, x='상품명')
-#sns.boxplot(x="영업일자", data=three)
-st.pyplot(fig)
+# fig = plt.figure(figsize=(10, 10))
+# sns.histplot(data=three, x='상품명')
+# #sns.boxplot(x="영업일자", data=three)
+# st.pyplot(fig)
 
 
 #5000~5019 12.25 크리스마스 음료 판매
