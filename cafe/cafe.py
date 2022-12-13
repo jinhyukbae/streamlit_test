6c87ba2ba8c24acad03b95f8a5558957f9b6a377
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 16:07:12 +0900

    1

--- cafe/cafe.py
+++ cafe/cafe.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('./cafe/data.csv', encoding='cp949')
 st.write(df.iloc[4588:5129])
+fig = plt.figure(figsize=(8, 4))
+sns.histplot(data=data, x='index')
+st.pyplot(fig)
 
 #5000~5019 12.25 크리스마스 음료 판매
 
