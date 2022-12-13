3c5367be16ee7b40ff5bd445e8d9b7a5547102e9
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 18:56:54 +0900

    1

--- cafe2/app.py
+++ cafe2/app.py
@@ -43,20 +43,20 @@ st.plotly_chart(fig1)
 
 st.write("# BEST")
 
-st.write("## 1.아메리카노 (HOT) 1215")
+st.write("## 1.아메리카노 (HOT) 1215잔 ")
 st.image("https://imagescdn.gettyimagesbank.com/500/201708/jv10960341.jpg")
-st.write("## 2.아메리카노 (ICE) 888")
+st.write("## 2.아메리카노 (ICE) 888잔 ")
 st.image("https://sitem.ssgcdn.com/29/96/11/item/1000329119629_i1_500.jpg")
-st.write("## 3.바닐라라떼 (HOT) 264")
+st.write("## 3.바닐라라떼 (HOT) 264잔 ")
 st.image("http://thumbnail.10x10.co.kr/webimage/image/basic600/352/B003520613.jpg?cmd=thumb&w=400&h=400&fit=true&ws=false")
 
 st.write("# WORST")
 
-st.write("## 1.그린티라떼 (HOT) 28 ")
+st.write("## 1.그린티라떼 (HOT) 28잔 ")
 st.image("https://t1.daumcdn.net/cfile/tistory/266ACC4A56C679952B")
-st.write("## 2.그린티라떼 (ICE) 36 ")
+st.write("## 2.그린티라떼 (ICE) 36잔 ")
 st.image("https://imagecdn.dpon.gift/images/merchandises/ediya%EB%85%B9%EC%B0%A8%EB%9D%BC%EB%96%BCICE.jpg")
-st.write("## 3.카페모카 (ICE) 38 ")
+st.write("## 3.카페모카 (ICE) 38잔 ")
 st.image("http://image.auction.co.kr/itemimage/19/a7/48/19a748d726.jpg")
 
 
