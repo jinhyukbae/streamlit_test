42fa3fb1f789ceb2c2dabcc9319ebda4cfdf8040
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 16:53:29 +0900

    1

--- cafe2/app.py
+++ cafe2/app.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('./cafe2/data.csv', encoding='cp949')
-st.write(df.iloc[5000:5020])
+three = st.write(df.iloc[5000:5020])
 #st.write(df.iloc[4588:5129])
 # two = df.iloc[4588:5129]
 
-# fig = plt.figure(figsize=(10, 10))
-# #sns.histplot(data=two, x='상품명')
-# sns.countplot(x="index", data=two)
-# plt.xticks()
-# st.pyplot(fig)
+fig = plt.figure(figsize=(10, 10))
+#sns.histplot(data=two, x='상품명')
+sns.countplot(x="영업일자", data=three)
+plt.xticks()
+st.pyplot(fig)
 
 
 #5000~5019 12.25 크리스마스 음료 판매
