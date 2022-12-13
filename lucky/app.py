dc472d19f5821c5397d4548e5895d2b966f2ae9a
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 12:32:25 +0900

    title

--- lucky/app.py
+++ lucky/app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import numpy as np
 
+# st.write() 마크 다운 
 
-st.write(
-    """
-    # 맛있는 감자탕
-    
-    """
-)
+st.title("조추첨 페이지")
 
-st.image("https://image.utoimage.com/preview/cp871385/2019/01/201901016652_500.jpg")
+# 추첨 대상 13명의 이름을 넣을 수 있는 text_input
+# 13명이 소속될 조 이름을 넣을 위치
+
+# <추첨 버튼>
+# 13개의 짝을 지어서 표시해줄 그래픽 
