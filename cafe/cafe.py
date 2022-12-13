b8bb10209494ae8e0be4376c7d69378f65ddbc13
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 16:01:28 +0900

    1

--- cafe/cafe.py
+++ cafe/cafe.py
@@ -5,4 +5,6 @@ import matplotlib as plt
 import seaborn as sns
 
 df = pd.read_csv('./cafe/data.csv', encoding='cp949')
-st.write(df.tail(150))
+st.write(df.iloc[5000:5019])
+
+#5000~5019 12.25 크리스마스 음료 판매
