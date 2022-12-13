81955e21294d8578e39409ee822e887de39ad951
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 17:04:43 +0900

    1

--- cafe2/app.py
+++ cafe2/app.py
@@ -10,17 +10,22 @@ st.write(df.iloc[5000:5020])
 #st.write(df.iloc[4588:5129])
 two = df.iloc[4588:5129]
 
-fig = plt.figure(figsize=(10, 10))
-#sns.histplot(data=three, x='상품명')
-#sns.histplot(x="상품명", y='판매수량', data=three)
-sns.histplot(x="상품명", y='판매수량', data=two)
-st.pyplot(fig)
+# fig = plt.figure(figsize=(10, 10))
+# #sns.histplot(data=three, x='상품명')
+# #sns.histplot(x="상품명", y='판매수량', data=three)
+# sns.histplot(x="상품명", y='판매수량', data=two)
+# st.pyplot(fig)
 
 
-x = two(['상품명'])
-y = two(['판매수량'])
-plt.title("Bar Chart")
-plt.bar(x,y)
+# x = two(['상품명'])
+# y = two(['판매수량'])
+# plt.title("Bar Chart")
+# plt.bar(x,y)
+# plt.show()
+
+two.plot.pie(autopct='%.2f%%')
+plt.title("선실별 승객 수 비율")
+plt.axis('equal')
 plt.show()
 
 #5000~5019 12.25 크리스마스 음료 판매
