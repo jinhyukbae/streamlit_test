7e0b6f180f663590d4fb45d74fe3206655d25114
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 17:37:29 +0900

    1

--- cafe2/app.py
+++ cafe2/app.py
@@ -10,5 +10,4 @@ df = pd.read_csv('./cafe2/data.csv', encoding='cp949')
 # #st.write(df.iloc[4588:5129])
 two = df.iloc[4588:5129]
 st.write(two)
-chart = alt.Chart(two).mark_circle().encode( x = '영업일자', y='상품명', color = 'species' )   ##알테어 
-st.altair_chart(chart, use_container_width=True)     #use_container_width=True  가로로 화면에 꽉 채워줌.
+st.bar_chart(two)
