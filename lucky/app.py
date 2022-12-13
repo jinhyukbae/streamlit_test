4dabcb7f5ebd363ce4584fc4f545435c1e3943fe
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-13 12:34:02 +0900

    header

--- lucky/app.py
+++ lucky/app.py
@@ -3,7 +3,8 @@ import numpy as np
 
 # st.write() 마크 다운 
 
-st.title("조추첨 페이지")
+st.title("조추첨 페이지") # 제목 
+st.header("여러분의 참여를 환영") #제목 다음으로 강조할 거
 
 # 추첨 대상 13명의 이름을 넣을 수 있는 text_input
 # 13명이 소속될 조 이름을 넣을 위치
