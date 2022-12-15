04dfe48d3e249a62d0c3037cc3ed0d50d9fdba8d
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-15 17:20:24 +0900

    1

--- ml_model/app.py
+++ ml_model/app.py
@@ -5,7 +5,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/d
 st.write(df)
 import joblib
 import os
-model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
+model_path = f"{os.path.dirname(os.path.abspath(__file__))}/first_model.pkl"
 model = joblib.load(model_path)
 st.write("## 선형 회귀 모델")
 st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
