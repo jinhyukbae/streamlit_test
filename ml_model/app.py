7297e258d1eaf594752687afb795129d47ebe31f
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-15 17:16:47 +0900

    1

--- ml_model/app.py
+++ ml_model/app.py
@@ -9,3 +9,5 @@ model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
 model = joblib.load(model_path)
 st.write("## 선형 회귀 모델")
 st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
+
+#
