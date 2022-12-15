86d55088891e137f5dbdf6f61ee4664db42442c1
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-15 17:14:38 +0900

    1

--- ml_model/app.py
+++ ml_model/app.py
@@ -1,11 +1,11 @@
+# streamlit 라이브러리 호출
 import streamlit as st
 import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
-
 import joblib
 import os
-
-model = joblib.load('ml_model/app.py')
-model_info = pd.Series(model.coef_, index = df.drop['expenses'].columns)
-st.write(model.coef_)
+model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
+model = joblib.load(model_path)
+st.write("## 선형 회귀 모델")
+st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
