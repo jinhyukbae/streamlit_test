193cff72c604e515db4aceedb5ba5ce60f98e6cf
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-15 17:11:43 +0900

    1

--- ml_model/app.py
+++ ml_model/app.py
@@ -4,6 +4,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/d
 st.write(df)
 
 import joblib
+import os
 
 model = joblib.load('ml_model/app.py')
 model_info = pd.Series(model.coef_, index = df.drop['expenses'].columns)
