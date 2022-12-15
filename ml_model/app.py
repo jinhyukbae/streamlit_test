2f0655856ed2d25b874901ebed72b60dfe63a571
Jin Hyuk Bae <115385678+jinhyukbae@users.noreply.github.com>
2022-12-15 17:24:10 +0900

    1

--- ml_model/app.py
+++ ml_model/app.py
@@ -10,31 +10,6 @@ model = joblib.load(model_path)
 st.write("## 선형 회귀 모델")
 st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
 
-# age : 나이
-st.number_input(
-    label="나이",
-    step=1,
-    value=30,
-    key='age'
-)
-# st.session_state['age']
-# st.write(st.session_state['age'])
-# sex : 성별
-st.radio(
-    label='성별',
-    options=["남성", "여성"],
-    index=0, # 기본 선택
-    key='sex'
-)
-# st.write(st.session_state['sex'])
-# bmi : 실수형
-st.number_input(
-    label="BMI",
-    step=0.1, # 실수형으로 받을 수 있게
-    value=25.0,
-    key='bmi'
-)
-st.write(st.session_state['bmi'])
 
 
 
